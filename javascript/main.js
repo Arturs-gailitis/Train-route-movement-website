ae41e3b9f75480ef07c818fc03cc28f267cd5ed1
Arturs-gailitis <artursgailitis2003@gmail.com>
2026-01-02 10:36:18 +0200

    nomaina bildes izmēru un ikonu lielumu kartē

--- javascript/main.js
+++ javascript/main.js
@@ -62,7 +62,7 @@ while (i < stations.length) {
     // nokonfigurē stacijas ikonu
     let stationIcon = L.icon({
         iconUrl: icons[i],
-        iconSize: [45, 48]
+        iconSize: [35, 38]
     })
 
     // ieliek kartē visas stacijas ar ikonu un tās nosaukumu papildus arī stacijas bildi
