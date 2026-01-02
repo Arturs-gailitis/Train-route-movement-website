18600f726430db629f9da1ba092fc0765208bda0
Arturs-gailitis <artursgailitis2003@gmail.com>
2026-01-02 08:33:52 +0200

    izlaboju centrēšanas stila kļūdas un footer

--- javascript/marsruti.js
+++ javascript/marsruti.js
@@ -3,12 +3,18 @@ const box = document.getElementById("maršrutuMeklēšanasSadaļa");
 const boxButton = document.getElementById("atcelt");
 const footer = document.querySelector("footer");
 
+// Ielādē footer 150 pikseļus no maršruta tabulas
+document.addEventListener("DOMContentLoaded", () => {
+    footer.style.position = "relative";
+    footer.style.marginTop = "150px";
+})
+
 // Parāda maršruta meklēšanas sadaļu
 button.addEventListener("click", () => {
     box.style.display = "block";
     button.style.display = "none";
     footer.style.marginTop = "200px";
-    footer.style.position = "relative"
+    footer.style.position = "relative";
 });
 
 // Aizver maršruta meklēšanas sadaļu
@@ -16,5 +22,4 @@ boxButton.addEventListener("click", () => {
     box.style.display = "none";
     button.style.display = "block";
     footer.style.marginTop = "-200px";
-    footer.style.position = "fixed"
 });
